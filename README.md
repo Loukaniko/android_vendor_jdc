d30619308da35dc6e56fca71a2b17a77d9a9aff9
dkati <dkatikaridis@gmail.com>
2018-07-12 12:47:08 +0300

    Remove JDC related stuff
    Change-Id: If159964165f0bca167c7e1bfcb1f47f44fa35ae1

--- README.md
+++
@@ -1,6 +0,0 @@
-==============
-Copyright 2015 - JDCTeam
---------------
-Addons/common files for ROMs
-
-WORK IN PROGRESS.
--- config/common.mk
+++ config/common.mk
@@ -1,57 +1,39 @@
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/jdc/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/jdc/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+    vendor/barbeque/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/barbeque/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/barbeque/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
 
 # Bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+    vendor/barbeque/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 # BSOD Killer
 PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/bin/bsod_killer:system/bin/bsod_killer
-
-# Official Changelog
-PRODUCT_COPY_FILES += \
-	vendor/jdc/Changelog.md:system/etc/Changelog.md
+    vendor/barbeque/prebuilt/common/bin/bsod_killer:system/bin/bsod_killer
 
 # Custom init script
 PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/etc/init.jdcteam.rc:root/init.jdcteam.rc
+    vendor/barbeque/prebuilt/common/etc/init.jdcteam.rc:root/init.jdcteam.rc
 
 # eMMC trim
 PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/bin/emmc_trim:system/bin/emmc_trim
+    vendor/barbeque/prebuilt/common/bin/emmc_trim:system/bin/emmc_trim
 
 # For keyboard gesture typing
 PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
-
-# LEDify
-PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/bin/ledify:system/bin/ledify
+    vendor/barbeque/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 
 # Log banner
 PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
+    vendor/barbeque/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/jdc/overlay/common
-
-# Packages
-PRODUCT_PACKAGES += \
-   Turbo \
-   Toolbox \
-   Mixplorer
-    
-# Proximity sensor reset
-PRODUCT_COPY_FILES += \
-    vendor/jdc/prebuilt/common/bin/proximity:install/bin/proximity
+DEVICE_PACKAGE_OVERLAYS += vendor/barbeque/overlay/common
 
 # Take a logcat
 #PRODUCT_COPY_FILES += \
-#vendor/jdc/prebuilt/common/bin/take_log:system/bin/take_log
+#vendor/barbeque/prebuilt/common/bin/take_log:system/bin/take_log
     
 #Substratum Verified
 PRODUCT_PROPERTY_OVERRIDES := \
